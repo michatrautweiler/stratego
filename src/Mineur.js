58ba05d004f63b0845f1bbe7a463d8270908edb1
micha <surf@Beas-iMac.fritz.box>
2020-01-03 23:04:53 +0100

    fix mineur vs bomben

--- src/Mineur.js
+++ src/Mineur.js
@@ -3,7 +3,7 @@ import { Figur }  from './Figur';
 export class Mineur extends Figur {
   
   schlage(gegner) {
-    if (gegner.gattung === "bombe") {
+    if (gegner.gattung === "bomben") {
       return 1; // gewonnen
     } else {
       return this.rang - gegner.rang;
